2fb624cab9f3783c5e28e82bd2b81df04cc0a22f
Hsuan-Yi,Lin <lovevul35j6@gmail.com>
2020-05-14 16:34:41 +0800

    PokerCard的客製化元件設定

--- app/src/main/java/com/example/poker/PokerCard.kt
+++ app/src/main/java/com/example/poker/PokerCard.kt
@@ -1,2 +1,13 @@
 package com.example.poker
 
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class PokerCard(context: Context,attrs:AttributeSet):ConstraintLayout(context,attrs) {
+//    呼叫建構值Context&屬性值AttributeSet，繼承ConstraintLayout
+    init {
+        View.inflate(context,R.layout.card,this)
+    }
+}
