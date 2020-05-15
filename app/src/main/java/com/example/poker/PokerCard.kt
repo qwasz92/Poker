c5d5a361a039a79c7526f2c49270249346ce3624
Hsuan-Yi,Lin <lovevul35j6@gmail.com>
2020-05-15 14:46:35 +0800

    設計花色處理 type的setImageResource如何取得值? 使用when語法 cordType的值如果是0->三葉草、1->黑桃、2->紅心、else方塊 最後在設定point的值，如果不是null，run以下的運算 point_left_top.text = this.toString() point_right_bottom.text = this.toString()

--- app/src/main/java/com/example/poker/PokerCard.kt
+++ app/src/main/java/com/example/poker/PokerCard.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
+import kotlinx.android.synthetic.main.card.view.*
 import kotlin.time.measureTimedValue
 
 class PokerCard(context: Context,attrs:AttributeSet):ConstraintLayout(context,attrs) {
@@ -17,9 +18,21 @@ class PokerCard(context: Context,attrs:AttributeSet):ConstraintLayout(context,at
 //    一個固定的值
     field = value
 //    一個區域變數=value值
-    val ponit = value?.run { value.rem(13) }
+    val ponit = value?.run { value.rem(13)+1 }
 //    定義ponit是一個value null.並執行value的餘數13(這裡指的是52張牌的每個數字)
     val cordType = value?.run { value/13 }
 //    定義cordType是一個value null.並執行value/13(這裡指的是撲克牌的四個顏色)
+    type.setImageResource(
+        when(cordType){
+            0-> R.drawable.clover
+            1->R.drawable.brick
+            2->R.drawable.heart
+            else -> R.drawable.spades
+        }
+    )
+    ponit?.run {
+        point_left_top.text = this.toString()
+        point_right_bottom.text = this.toString()
+    }
 }
 }
