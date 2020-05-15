be31c6fedd89a72a924c331b7c43e9ad1517c936
Hsuan-Yi,Lin <lovevul35j6@gmail.com>
2020-05-15 14:15:51 +0800

    設計PokerCard的set方法 定義var value是一個空數字值 在使用set方法來取得value值 因set方法無法取得上面所定義的value值，所以要透過field的區域變數來取得 最後在定義 1.ponit，來隨機取得52張牌的每個數字value 2.cordType，來隨機取得撲克牌的四個顏色其中一個花色

--- app/src/main/java/com/example/poker/PokerCard.kt
+++ app/src/main/java/com/example/poker/PokerCard.kt
@@ -4,10 +4,22 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
+import kotlin.time.measureTimedValue
 
 class PokerCard(context: Context,attrs:AttributeSet):ConstraintLayout(context,attrs) {
 //    呼叫建構值Context&屬性值AttributeSet，繼承ConstraintLayout
     init {
         View.inflate(context,R.layout.card,this)
     }
+    var value:Int? = null
+//    牌值value是數字null
+    set(value){
+//    一個固定的值
+    field = value
+//    一個區域變數=value值
+    val ponit = value?.run { value.rem(13) }
+//    定義ponit是一個value null.並執行value的餘數13(這裡指的是52張牌的每個數字)
+    val cordType = value?.run { value/13 }
+//    定義cordType是一個value null.並執行value/13(這裡指的是撲克牌的四個顏色)
+}
 }
