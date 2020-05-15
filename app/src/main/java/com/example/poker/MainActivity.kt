de6e83d3565de636f67ebc8c92ab004308cfb7b5
Hsuan-Yi,Lin <lovevul35j6@gmail.com>
2020-05-15 14:50:07 +0800

    當按下fab的按鈕執行以下內容 當按下按鈕，讓poker的value值隨機跳。如何隨機跳? 使用Random語法，讓他的值設定為0-52中亂數跳

--- app/src/main/java/com/example/poker/MainActivity.kt
+++ app/src/main/java/com/example/poker/MainActivity.kt
@@ -7,6 +7,8 @@ import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,8 +18,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+       poker.value = Random.nextInt(52)
         }
     }
 
